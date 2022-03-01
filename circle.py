366b7cbd124d15a3931942616fec1c73626300dd
Worse2Worst <wasuwach@hotmail.com>
2022-03-01 17:58:35 +0900

    Done

---
+++ circle.py
@@ -0,0 +1,12 @@
+from math import pi
+
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def get_area(self):
+        return pi * self.radius * self.radius
+
+    def get_perimeter(self):
+        return 2.0 * pi * self.radius
